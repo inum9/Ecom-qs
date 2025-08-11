015d7f22436257aadd5ea96ab1063943bc3efb8b
[inum9] <inum9@outlook.com>
2025-08-11 22:21:19 +0530

    debugged_3

--- backend/src/middleware/Auth.middleware.js
+++ backend/src/middleware/Auth.middleware.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 import  User from "../model/user.model.js";
 import { ApiError } from "../utils/ApiError.js";
-import { asyncHandler } from "../utils/asyncHandler.js";
+import { asyncHandler } from "../utils/AsyncHandler.js";
 
 export const verifyJWT = asyncHandler(async(req, _, next) => {
     try {
