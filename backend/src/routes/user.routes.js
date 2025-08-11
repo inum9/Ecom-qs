56f701fbe5781d91391aebde55383e2ab560fb92
[inum9] <inum9@outlook.com>
2025-08-11 21:06:16 +0530

    implemented the  authentication system

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,7 +1,12 @@
 import {Router} from "express"
-import { loginUser, registerUser } from "../controller/user.controller.js";
+import { loginUser, logoutUser, registerUser } from "../controller/user.controller.js";
+import { verifyJWT } from "../middleware/Auth.middleware.js";
+import { app } from "../app.js";
 
 const userRoutes= Router();
 userRoutes.route("/register").post(registerUser);
 userRoutes.route("/login").get(loginUser);
+
+///secured routes
+userRoutes.route("/logout",verifyJWT,logoutUser)
 export {userRoutes};
